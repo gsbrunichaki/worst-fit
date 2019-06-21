e076d0ff1062ed60bc0bfbb6e8f7208cc091c3d4
gabriel.brunichaki <gabriel.brunichaki@acad.pucrs.br>
2019-06-21 17:39:37 -0300

    Comparable in Partition class (by size)

--- App.java
+++ App.java
@@ -1,9 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 /**
  * App
  */
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        try {
+            FileReader textFile = new FileReader("test1.txt");
+            BufferedReader readFile = new BufferedReader(textFile);
+
+            // Ignora a primeira linha
+            readFile.readLine();
+
+            Memory memory = new Memory(readFile.readLine(), readFile.readLine());
+
+            Manager manager = new Manager(memory);
+
+            while (readFile.ready()) {
+                lineToRequest(manager, readFile.readLine());
+            }
+
+            manager.receiveRequest(new Request("S", "250"));
+            manager.receiveRequest(new Request("S", "100"));
+            manager.receiveRequest(new Request("S", "200"));
+            manager.receiveRequest(new Request("S", "400"));
+
+            manager.test();
+
+            textFile.close();
+        } catch (IOException e) {
+            System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+        }
     }
 }
--- Partition.java
+++ Partition.java
@@ -1,16 +1,18 @@
 /**
  * Partition
  */
-public class Partition {
+public class Partition implements Comparable<Partition> {
     int id;
     int start;
     int end;
+    int size;
     boolean occupied;
 
     public Partition(int id, int start, int end) {
         this.id = id;
         this.start = start;
         this.end = end;
+        this.size = end - start;
         this.occupied = false;
     }
 
@@ -34,12 +36,31 @@ public class Partition {
         this.end = end;
     }
 
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
     public boolean isOccupied() {
         return this.occupied;
     }
 
     public void setOccupied(boolean occupied) {
         this.occupied = occupied;
+    }
+
+    @Override
+    public int compareTo(Partition partition) {
+        if (this.size < partition.getSize()) {
+            return -1;
+        } else if (this.size > partition.getSize()) {
+            return 1;
+        }
+
+        return 0;
     }
 
     @Override
